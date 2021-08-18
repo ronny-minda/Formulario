309cca445f56cc3fd46709c4c54d83a47917bb14
ronny-minda <ronny.michael.minda.vera@gmail.com>
2021-08-17 21:34:45 -0500

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {H1, H2} from './Styles';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <H1>hola</H1>
+      <H2>hola2</H2>
     </div>
   );
 }
---
+++ src/Styles.js
@@ -0,0 +1,14 @@
+
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+    height: 50px;
+    width: 50px;
+    background-color: red;
+`;
+
+export const H2 = styled.h2`
+    height: 50px;
+    width: 50px;
+    background-color: blue;
+`;
